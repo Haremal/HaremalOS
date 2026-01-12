6758f1e43d25bc95626787cbb4b8239f960c3905
Haremal <118294158+Haremal@users.noreply.github.com>
2026-01-12 14:55:19 +0100

    Update system_apps.sh

--- system_apps.sh
+++ system_apps.sh
@@ -7,7 +7,7 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm --needed \
 	mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
 	libva-mesa-driver lib32-libva-mesa-driver \
-	networkmanager bluez bluez-utils \
+	networkmanager bluez bluez-utils glib2 fontconfig \
 	xdg-desktop-portal xdg-desktop-portal-hyprland gnome-keyring \
 	xdg-desktop-portal-gtk xdg-utils hyprland-qt-support \
 	wayland wayland-protocols xorg-server-xwayland python \
@@ -27,24 +27,25 @@ pacman -S --noconfirm --needed \
 	base-devel git cmake ninja sdbus-cpp rust \
     dotnet-sdk jdk-openjdk lua-language-server
 
-# --- 4. TEMPORARY USER CREATION ---
-useradd -m -G wheel -s /bin/bash builder
-echo "builder ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/builder
+# --- 4. TEMPORARY GHOST ---
+echo "nobody ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nobody-build
 
 # --- 5. AUR (Paru & Themes) ---
-# We build the theme components as the temp user
-su - builder -c "git clone https://aur.archlinux.org/paru-bin.git ~/paru && cd ~/paru && makepkg -si --noconfirm"
-su - builder -c "paru -S --noconfirm --skipreview --needed bibata-cursor-theme-bin python-pywal hyprpanel-git hyprlauncher-git tty-clock hyprshot jetbrains-toolbox"
-
+mkdir -p /tmp/paru
+chown -R nobody:nobody /tmp/paru
+su -s /bin/bash nobody -c "cd /tmp/paru && git clone https://aur.archlinux.org/paru-bin.git . && makepkg -si --noconfirm"
+# Set a temporary cache directory for the 'nobody' user
+su -s /bin/bash nobody -c "paru -S --noconfirm --skipreview --needed --cachedir /tmp/paru_cache \
+    bibata-cursor-theme-bin python-pywal hyprpanel-git hyprlauncher-git tty-clock hyprshot jetbrains-toolbox"
+	
 # --- 6. INSTALL CHOSEN APPS ---
 mkdir -p /opt
 [[ "$I_STEAM" == "y" ]] && pacman -S --noconfirm --needed steam
 [[ "$I_BLENDER" == "y" ]] && pacman -S --noconfirm --needed blender
 [[ "$I_OBS" == "y" ]] && pacman -S --noconfirm --needed obs-studio
-[[ "$I_UNITY" == "y" ]] && su - builder -c "ACCEPT_EULA=Y paru -S --noconfirm --skipreview --needed unityhub"
-[[ "$I_REAPER" == "y" ]] && su - builder -c "paru -S --noconfirm --skipreview --needed reaper-bin"
+[[ "$I_UNITY" == "y" ]] && mkdir -p /tmp/unity_home && chown nobody:nobody /tmp/unity_home && HOME=/tmp/unity_home su -s /bin/bash nobody -c "ACCEPT_EULA=Y paru -S --noconfirm --skipreview --needed --cachedir /tmp/paru_cache unityhub"
+[[ "$I_REAPER" == "y" ]] && su -s /bin/bash nobody -c "paru -S --noconfirm --skipreview --needed --cachedir /tmp/paru_cache reaper-bin"
 
 # --- 7. CLEANUP ---
-pkill -9 -u builder || true
-userdel -rf builder || true
-rm -f /etc/sudoers.d/builder
+rm /etc/sudoers.d/nobody-build
+rm -rf /tmp/paru /tmp/paru_cache /tmp/unity_home
