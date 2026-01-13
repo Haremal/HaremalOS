9cefcd551e674f1384bfd5ab563f39d58c6c7e93
Haremal <118294158+Haremal@users.noreply.github.com>
2026-01-13 22:09:32 +0100

    Update system_apps.sh

--- system_apps.sh
+++ system_apps.sh
@@ -6,25 +6,25 @@ pacman -Syu --noconfirm
 # Get the hardware ready before the heavy apps
 pacman -S --noconfirm --needed \
 	mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
-	libva-mesa-driver lib32-libva-mesa-driver \
+	libva-mesa-driver lib32-libva-mesa-driver libva-utils \
+	pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber \
 	networkmanager bluez bluez-utils glib2 fontconfig \
 	xdg-desktop-portal xdg-desktop-portal-hyprland gnome-keyring \
 	xdg-desktop-portal-gtk xdg-utils hyprland-qt-support \
 	wayland wayland-protocols xorg-server-xwayland python \
 	polkit polkit-kde-agent gsettings-desktop-schemas \
-	qt5-wayland qt6-wayland
+	qt5-wayland qt6-wayland base-devel gcc cmake socat git
 	
 # --- 2. THE HAERMALOS STACK (Core Apps) ---
 pacman -S --noconfirm --needed \
-	hyprland ags-hyprpanel-git hyprpaper hypridle hyprlock hyprlauncher hyprshot \
-	ly paru pipewire pipewire-pulse wireplumber mpvpaper mpv grim slurp \
-	wezterm neovim yazi fastfetch cava cmatrix tty-clock \
-	ffmpeg fd ripgrep p7zip unzip zip libnotify wl-clipboard \
-	playerctl lm_sensors papirus-icon-theme python-pywal bibata-cursor-theme
+	ly hyprland swww hypridle hyprlock \
+	foot chafa neovim yazi fastfetch cava cmatrix tty-clock \
+	paru ffmpeg fd ripgrep p7zip unzip zip libnotify grim slurp wl-clipboard mpv \
+	eww-wayland-git brightnessctl playerctl lm_sensors  papirus-icon-theme bibata-cursor-theme python-pywal
 	
 # --- 3. LANGUAGES & DEV TOOLS ---
 pacman -S --noconfirm --needed \
-	base-devel git cmake ninja sdbus-cpp rust \
+	ninja nlohmann-json sdbus-cpp rust \
     dotnet-sdk jdk-openjdk lua-language-server
 
 # --- 4. CHOSEN APPS ---
