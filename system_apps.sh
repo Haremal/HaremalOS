ceb0ef309ca92d4fe0d16246e1c279dbbbf75825
Haremal <118294158+Haremal@users.noreply.github.com>
2026-01-13 19:01:15 +0100

    Update system_apps.sh

--- system_apps.sh
+++ system_apps.sh
@@ -16,8 +16,8 @@ pacman -S --noconfirm --needed \
 	
 # --- 2. THE HAERMALOS STACK (Core Apps) ---
 pacman -S --noconfirm --needed \
-	hyprland ags-hyprpanel-git hyprpaper hypridle hyprlock hyprshot hyprlauncher \
-	ly paru pipewire pipewire-pulse wireplumber mpv grim slurp \
+	hyprland ags-hyprpanel-git hyprpaper hypridle hyprlock hyprlauncher hyprshot \
+	ly paru pipewire pipewire-pulse wireplumber mpvpaper mpv grim slurp \
 	wezterm neovim yazi fastfetch cava cmatrix tty-clock \
 	ffmpeg fd ripgrep p7zip unzip zip libnotify wl-clipboard \
 	playerctl lm_sensors papirus-icon-theme python-pywal bibata-cursor-theme
